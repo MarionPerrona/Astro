aafaeb5df9a95d44d0d7140c1097e04105392429
MarionPerrona <marion.perrona@etu.unilim.fr>
2024-01-08 12:18:09 +0100

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: "Mon voyage d'apprentissage d'Astro",
-    site: 'https://mon-site-de-blog.netlify.app',
+    site: 'https://aesthetic-boba-a6267d.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>fr-fr</language>`,
   });
